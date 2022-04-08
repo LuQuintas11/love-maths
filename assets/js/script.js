1db2edd8f3d84adbd23083e849e45ac53fbf037f
LuQuintas11 <luciaquintas14@gmail.com>
2022-04-08 20:19:21 +0000

    add listeners to the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 console.log("Connected1")
 
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type")==="submit"){
+                alert("you clicked Submit");
+            }else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+
+        })
+    }
+})
+
 function runGame(){
 
 }
@@ -12,3 +28,6 @@ function calculateCorrectAnswer(){
 
 }
 
+function incrementScore(){
+
+}
